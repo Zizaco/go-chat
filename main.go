6477a73704c9004663f8a783d48486309e07576b
Zizaco <zizaco@gmail.com>
2016-01-10 12:13:31 -0200

    Separated templates from the source code

--- main.go
+++ main.go
@@ -3,22 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-            <html>
-                <head>
-                    <title>Chat</title>
-                    <body>
-                        Let's chat!
-                    </body>
-                </head>
-            </html>
-        `))
-	})
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	//start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
